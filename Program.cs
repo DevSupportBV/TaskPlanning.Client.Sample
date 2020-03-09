b7b65a53e58500163c7de8bec967374ae05031a0
Piet Eckhart <piet@softwaretogo.nl>
2020-03-09 11:26:43 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using AutoPlanning.Models;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TaskPlanning.Client.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var accessKey = File.ReadAllText("AccessKey.txt");
+
+            var client = TaskPlanningClient.Create(accessKey);
+
+            var request = new PlanningRequest();
+
+            PlanningTask task = await client.Plan(request);
+        }
+    }
+}
